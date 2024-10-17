638e181ebaa141e2aae8c8ab19aa7488e660aa23
Newton <5769156+iamnewton@users.noreply.github.com>
2024-10-16 18:00:55 -0700

    feat: update with new project pages (#6)
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,10 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
+					label: "Projects",
+					autogenerate: { directory: "projects" },
+				},
+				{
 					label: "Contributing",
 					items: [
 						"contributing",

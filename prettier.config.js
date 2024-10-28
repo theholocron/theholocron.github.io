dd86c5374ac9761ba302a70df1601ad7202e1750
Newton <5769156+iamnewton@users.noreply.github.com>
2024-10-28 14:29:59 -0700

    chore: update to add in new workflows
    Fixes: #13
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+import theholocron from "@theholocron/prettier-config";
+
+/**
+ * @see https://prettier.io/docs/en/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+	...theholocron,
+};
+
+export default config;

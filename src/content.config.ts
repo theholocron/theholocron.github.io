edc18c578a2636f1721592287d6668c7d4c3ad68
Newton <5769156+iamnewton@users.noreply.github.com>
2026-07-29 15:58:10 -0700

    feat: upgrade to Astro 7 + Starlight 0.41, wire @theholocron/clients-docs
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { docsSchema } from "@astrojs/starlight/schema";
+import { defineCollection } from "astro:content";
+import { fileURLToPath } from "node:url";
+import { docsLoader } from "./loaders/docs-loader.ts";
+
+export const collections = {
+	docs: defineCollection({
+		loader: docsLoader([
+			{
+				dir: fileURLToPath(new URL("content/docs", import.meta.url)),
+				slug: "",
+			},
+			{
+				package: "@theholocron/clients-docs",
+				slug: "projects/clients",
+			},
+		]),
+		schema: docsSchema(),
+	}),
+};
